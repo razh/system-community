49269f02cbc0de6b60de218aa1f45ef0ea88749a
Raymond Zhou <somecrazyidiot@gmail.com>
2014-01-13 14:08:57 -0500

    Add box/transform/transformOrigin logic. Add configuration.

--- js/robot.js
+++ js/robot.js
@@ -53,11 +53,119 @@
         faceEl.style.marginTop  = -halfDepth + 'px';
       }
 
+      if ( back || front ) {
+        faceEl.style.width =  width  + 'px';
+        faceEl.style.height = height + 'px';
+
+        faceEl.style.marginLeft = -halfWidth  + 'px';
+        faceEl.style.marginTop =  -halfHeight + 'px';
+      }
+
+      if ( left || right ) {
+        faceEl.style.width =  depth  + 'px';
+        faceEl.style.height = height + 'px';
+
+        faceEl.style.marginLeft = -halfDepth  + 'px';
+        faceEl.style.marginTop =  -halfHeight + 'px';
+      }
+
       if ( top    ) { transform = 'translate3d(0, ' + -halfHeight + 'px, 0) rotateX( 90deg)'; }
+      if ( bottom ) { transform = 'translate3d(0, ' +  halfHeight + 'px, 0) rotateX(-90deg)'; }
+
+      if ( back  ) { transform = 'translate3d(0, 0, ' + -halfDepth + 'px) rotateY(180deg)'; }
+      if ( front ) { transform = 'translate3d(0, 0, ' +  halfDepth + 'px) rotateY(  0deg)'; }
+
+      if ( left  ) { transform = 'translate3d(' + -halfWidth + 'px, 0, 0) rotateY(-90deg)'; }
+      if ( right ) { transform = 'translate3d(' +  halfWidth + 'px, 0, 0) rotateY( 90deg)'; }
+
+      faceEl.style.webkitTransform = transform;
+      faceEl.style.transform = transform;
     });
+  }
+
+  function setTranslate3D( el, x, y, z ) {
+    var transform = 'translate3d(' +
+      x + 'px, ' +
+      y + 'px, ' +
+      z + 'px';
+
+    el.style.webkitTransform = transform;
+    el.style.transform = transform;
+  }
+
+  function setTransformOrigin( el, x, y, z ) {
+    var transformOrigin = x + 'px, ' +
+      y + 'px, ' +
+      z + 'px';
+
+    el.style.webkitTransformOrigin = transformOrigin;
+    el.style.transformOrigin = transformOrigin;
   }
 
   var chestEl = document.querySelector( '#chest' );
   appendBox( chestEl );
   setDimensions( chestEl, 200, 200, 200 );
+
+  var config = (function() {
+    var head = {
+      width: 50,
+      height: 50,
+      depth: 50
+    };
+
+    var chest = {
+      width: 200,
+      height: 200,
+      depth: 200
+    };
+
+    var arm = {
+      width: 20,
+      height: 100,
+      depth: 20
+    };
+
+    var leg = {
+      width: 20,
+      height: 40,
+      depth: 20
+    };
+
+    var headOffsetY = 0.5 * ( head.height + chest.height );
+    var armOffsetX = 0.5 * ( chest.width + arm.width );
+
+    var legOffsetX = 0.25 * chest.width;
+    var legOffsetY = 0.5 * ( leg.height + chest.height );
+
+    return {
+      head: {
+        dimensions: [ head.width, head.height, head.depth ],
+        translate3d: [ 0, -headOffsetY, 0 ]
+      },
+
+      chest: {
+        dimensions: [ chest.width, chest.height, chest.depth ]
+      },
+
+      arm: {
+        dimensions: [ arm.width, arm.height, arm.depth ],
+        transformOrigin: [ 0, -0.5 * arm.height ]
+      },
+
+      'arm-left': [ armOffsetX ],
+      'arm-right': [ -armOffsetX ],
+
+      leg: {
+        dimensions: [ leg.width, leg.height, leg.depth ],
+        translate3d: [ 0, legOffsetY, 0 ],
+        transformOrigin: [ 0, -0.5 * leg.height ]
+      },
+
+      'leg-left': [ legOffsetX ],
+      'leg-right': [ -legOffsetX ]
+    };
+  }) ();
+
+  var armLeftEl = document.querySelector( '#arm-left' );
+  appendBox( armLeftEl );
 }) ( window, document );
